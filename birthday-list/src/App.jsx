3155bad75fd30f314a343801582ac34c843cffd9
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-10 17:43:16 +0530

    import Component and code funcnality of button

--- birthday-list/src/App.jsx
+++ birthday-list/src/App.jsx
@@ -1,4 +1,19 @@
+import data from './data'
+import List from './List';
+import {useState} from 'react'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [item, setItem] = useState(data)
+
+  const handleClick = () => {
+    setItem([])
+  }
+  return <main>
+    <section className='container'>
+      <h3>{item.length} Birthdays Today</h3>
+      <List item={item}/>
+      <button type='button' className="btn btn-block" onClick={handleClick}>Clear all</button>
+  </section>
+  </main>
 };
 export default App;
