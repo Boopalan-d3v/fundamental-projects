57f3bc254e0f2fb3d1b3b7bb4477f1e5175f40f4
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-16 19:06:10 +0530

    App component

--- alternative-questions/src/App.jsx
+++ alternative-questions/src/App.jsx
@@ -3,9 +3,17 @@ import data from './data'
 import Questions from './Questions'
 function App() {
   const [questions, setQuestions] = useState(data)
+  const [activeid, setActiveid] = useState(null)
+  const toggleQuestion = (id) => {
+    console.log(id)
+    const newActiveid = id === activeid ? null : id;
+    setActiveid(newActiveid)
+  }
   return (
     <main>
-      <Questions questions={questions} />
+      <Questions questions={questions}
+      activeid={activeid}
+      toggleQuestion={toggleQuestion} />
     </main>
   )
 }
