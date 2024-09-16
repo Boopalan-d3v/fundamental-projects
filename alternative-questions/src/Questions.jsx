e138ff1e08744e11956cbb55ef93257828ef0e4c
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-16 19:07:10 +0530

    contains Question component

--- alternative-questions/src/Questions.jsx
+++ alternative-questions/src/Questions.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SingleQuestion from './SingleQuestion'
 
-const Questions = ({questions}) => {
+const Questions = ({questions , activeid, toggleQuestion}) => {
   return (
-    <section>
+    <section className='container'>
+        <h1>Questions</h1>
         {questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question}/>
+            return <SingleQuestion key={question.id} {...question} 
+            activeid={activeid} toggleQuestion={toggleQuestion}/>
         })}
     </section>
   )
