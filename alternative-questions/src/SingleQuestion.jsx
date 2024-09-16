f4ebc2de21e8de3c359f6088c2aece03f49380ad
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-16 19:07:35 +0530

    SingleQuestion component

--- alternative-questions/src/SingleQuestion.jsx
+++ alternative-questions/src/SingleQuestion.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
 import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
-const SingleQuestion = ({ title, info}) => {
-    const [showData, setShowData] = useState(false)
+const SingleQuestion = ({ id, title, info, activeid, toggleQuestion}) => {
+    const isActive = id === activeid
   return (
-    <main>
-        <article className='container'>
-            <header>
-                <h3>Questions</h3>
-            </header>
+    
+    <article className='question'>
+        <header>
             <h5>{title}</h5>
-            {showData && <p>{info}</p>}
-            <button type='button' onClick={() => setShowData(!showData)}>
-                {showData ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+            <button type='button' className='question-btn' onClick={() => toggleQuestion(id)}>
+                {isActive ? <AiOutlineMinus/> : <AiOutlinePlus/>}
             </button>
-        </article>
-    </main>
+        </header>            
+        {isActive && <p>{info}</p>} 
+    </article>
   )
 }
-
 export default SingleQuestion
