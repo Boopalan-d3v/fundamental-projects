7102869679c6d21bca3ce7d0a1ab4ceb114f63b6
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-17 22:29:05 +0530

    Set() method to filter unique values from object

--- menu-section/src/App.jsx
+++ menu-section/src/App.jsx
@@ -2,8 +2,16 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from './data'
 import Menu from "./Menu";
+
+//const tempCatgory = menu.map((item)=> item.category)
+//const unique = ['all', ...new Set(tempCatgory)]
+//console.log(unique);
+
+const allCategories = ['all', new Set(menu.map((item)=> item.category))]
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
   return <main>
     <section className="menu">
       <Title text='our menu'/>
