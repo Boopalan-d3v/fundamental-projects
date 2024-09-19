c1791b12dfc122171c6b30a0df83ea311e879731
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-19 12:20:26 +0530

    Job component contains jobInformation

---
+++ tabs/src/Job.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Duties from './Duties'
+
+const Job = ({jobs, currentItem}) => {
+    const { title, dates, duties, company} = jobs[currentItem]
+  return (
+    <article className='job-info'>
+        <h3>{title}</h3>
+        <span className="job-company">
+            {company}
+        </span>
+        <p className="job-date">
+            {dates}
+        </p>
+        <Duties duties = {duties}/>
+    </article>  
+  )
+}
+
+export default Job
