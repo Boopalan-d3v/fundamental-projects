d2d7d2192b7ca3dc9903300f2cf537a6a6aa714f
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-19 13:08:58 +0530

    fix small error on btn classname

--- tabs/src/BtnContainer.jsx
+++ tabs/src/BtnContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BtnContainer = ({ jobs, currentItem, setCurrentItem}) => {
   return (
-    <div className="BtnContainer">
+    <div className="btn-container">
         {jobs.map((item, index) => {
             return <button key={item.id} className={index === currentItem ? 'job-btn active-btn':'job-btn'} onClick={() => setCurrentItem(index)}> {item.company} </button>
         })}
