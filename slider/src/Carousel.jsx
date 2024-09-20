fe8d4481f7e452b62680bb272b1e64f5180375e9
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-20 17:02:45 +0530

    fix prevSlide & nextSlide function functionality

--- slider/src/Carousel.jsx
+++ slider/src/Carousel.jsx
@@ -4,21 +4,32 @@ import { useState} from 'react'
 import {FiChevronLeft , FiChevronRight} from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 const Carousel = () => {
-  const [dataList, setDataList] = useState(shortList)
+  const [dataList, setDataList] = useState(longList)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
   const prevSlide = () => {
-
+    setCurrentPerson((oldPerson) =>{
+      const result = (oldPerson - 1 + dataList.length) % dataList.length
+      return result
+    } )
   }
 
   const nextSlide = () => {
-
+    setCurrentPerson((oldPerson) =>{
+      const result = (oldPerson + 1) % dataList.length
+      return result
+    } )
   }
 
   return (
     <section className='slider-container'>
-      {dataList.map((people) => {
+      {dataList.map((people, peopleIndex) => {
         const { id, name, image,title, quote} = people
-        return <article className="slide" key={id}>
+        return <article className="slide" 
+        style={{transform:`translateX(${100 * (peopleIndex - currentPerson)}%)`, 
+          opacity:peopleIndex === currentPerson?1:0, 
+          visibility:peopleIndex === currentPerson? 'visible' : 'hidden'}}
+         key={id}>
           <img src={image} alt={name} className='person-img'/>
           <h5 className='name'>{name}</h5>
           <p className="title">{title}</p>
