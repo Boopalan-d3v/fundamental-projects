cfbb9dc192a1cac78d3660f942ce3a83eae9dbee
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-22 22:38:06 +0530

    render item based on input value

--- lorem-ipsum/src/App.jsx
+++ lorem-ipsum/src/App.jsx
@@ -1,9 +1,41 @@
 import {useState} from 'react'
+import data from './data';
 const App = () => {
-  const [ value, setValue] = useState(1)
+  const [ count, setCount] = useState(1)
+  const [ text, setText] = useState([])
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    setText(data.slice(0,amount))
+  }
+
   return (
-    <section>
-      {value}
+    <section className='section-center'>
+      <h2 className='title'>Tired of Boring Lorem Ipsum</h2>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraph:</label>
+        <input 
+          type='number' 
+          id='amount' 
+          name='amount' 
+          min='1' 
+          step='1' 
+          max='8' 
+          value={count} 
+          onChange={handleChange}>
+        </input>
+        <button type='submit' className='btn'>generate</button>
+        <article className='lorem-text'>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>
+          })}
+        </article>
+      </form>
     </section>
   );
 };
