953c6022b9c00eaf7c3c4e9956371ebf128bca7f
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-23 17:30:36 +0530

    fix try catch func

--- color-generater/src/App.jsx
+++ color-generater/src/App.jsx
@@ -6,9 +6,19 @@ import { ToastContainer, toast} from 'react-toastify'
 const App = () => {
   const [ colors, setColors] = useState(new Values('#f15025').all(10));
  
+  const addColor = (input) =>{
+    try{
+      const newColors = new Values(input).all(10);
+      setColors(newColors);
+
+    } catch (error) {
+      toast.error(error.message);      
+    }
+  }
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor}/>
       <ColorList colors={colors} />
       <ToastContainer position="top-center"/>
     </main>
