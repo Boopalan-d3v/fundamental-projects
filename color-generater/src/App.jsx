40e9f383695aecb4e90c6b23b870ef8b0951fe28
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-23 14:56:48 +0530

    import ToastContainer and toast from react-toastify

--- color-generater/src/App.jsx
+++ color-generater/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import ColorList from "./Components/ColorList";
 import Form from "./Components/Form";
 import Values from 'values.js'
+import { ToastContainer, toast} from 'react-toastify'
 const App = () => {
   const [ colors, setColors] = useState(new Values('#f15025').all(10));
+ 
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center"/>
     </main>
   );
 };
