e64bbcabcdb163746c57ef0b0d80cf6c8c3ef593
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-23 17:29:16 +0530

    fix className in p tag to show hex value

--- color-generater/src/Components/SingleColor.jsx
+++ color-generater/src/Components/SingleColor.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 
 const SingleColor = ({color, index}) => {
     const { hex, weight }  = color
+
+    const saveToClipboard = async() =>{
+        if(navigator.clipboard){
+            try {
+               await navigator.clipboard.writeText(`#${hex}`)
+               toast.success('Copied')
+            } catch (error) {
+                toast.error('failed to copy')
+            }
+        }else{
+            toast.error('Clipboard access not available')
+        }
+    }
   return (
-    <article className={index > 10 ? 'color color-light' : 'color'} style={{background:`#${hex}`}}>
-        <p className='present-valur'>
-            {weight}%
-        </p>
-        <p className='color-value'>
-            #{hex}
-        </p>
+    <article 
+        className={index > 10 ? 'color color-light' : 'color'} 
+        style={{background:`#${hex}`}} 
+        onClick={saveToClipboard}
+    >
+        <p className='present-value'> {weight}% </p>
+        <p className='color-value'> #{hex} </p>
     </article>
   )
 }
