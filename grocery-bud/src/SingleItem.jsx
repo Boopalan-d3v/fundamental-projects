49cebf7794c0a04dbc8a737f59b39c2a7d4e4bb6
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-24 20:07:24 +0530

    fix checkedState and editItem functionality

---
+++ grocery-bud/src/SingleItem.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+ 
+const SingleItem = ({item, removeItem, editItem}) => {
+    const {id, name, completed} = item
+
+    /* const handleCheck = () => {
+        if(!isChecked){
+            setIsChecked({completed:true})
+        }
+        return;
+    } */
+
+  return (
+    <div className='single-item'>
+        <input type="checkbox" checked={completed} onChange={() => editItem(id)}/>
+        <p style={{textTransform: 'capitalize', textDecoration: completed && 'line-through'}}>{name}</p>
+        <button type='button' className='btn remove-btn' onClick={() => removeItem(id)}>Delete</button>
+    </div>
+  )
+}
+
+export default SingleItem
