3b3a1e671e86266b37111930d2466338b7d265c9
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-24 22:24:49 +0530

    add useRef and useEffect hook

--- grocery-bud/src/Form.jsx
+++ grocery-bud/src/Form.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {toast} from 'react-toastify'
 const Form = ({addItem}) => {
 
-    const [itemInput, setItemInput] = useState([])
+    const [itemInput, setItemInput] = useState('')
+
+    const refContainer = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault(); 
-        
         if (!itemInput) {
             toast.error('Please provide a value')
-            return}
+            return;
+        }
         addItem(itemInput)
         setItemInput('') 
     }
+
+    useEffect(()=>{
+        refContainer.current.focus()
+    },[])
 
   return (
     <form className="item-form" onSubmit={handleSubmit}>
@@ -23,9 +29,10 @@ const Form = ({addItem}) => {
             className='form-input' 
             value={itemInput} 
             onChange={(e) => setItemInput(e.target.value)} 
+            ref={refContainer}
         />
-        </div>
         <button type='submit' className='btn'>Submit</button>
+        </div>
     </form>
   )
 }
