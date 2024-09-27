b5450899593e81084882b7a029dc28ee8f50add7
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 22:18:42 +0530

    import required components

--- strapi-submenus/src/App.jsx
+++ strapi-submenus/src/App.jsx
@@ -1,4 +1,14 @@
-const App = () => {
-  return <h2>Strapi Starter</h2>;
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Submenu from "./Components/Submenu";
+
+const App = () => {  
+  return <main>
+    <Navbar/>
+    <Hero/>
+    <Sidebar/>
+    <Submenu/>
+  </main>;
 };
 export default App;
