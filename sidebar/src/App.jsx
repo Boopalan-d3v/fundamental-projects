79935e7d122b0fa7168a131c675ba0d5e16b22e5
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 10:57:32 +0530

    import Home, Model and Sidebar component

--- sidebar/src/App.jsx
+++ sidebar/src/App.jsx
@@ -1,4 +1,12 @@
+import Home from "./Components/Home";
+import Model from "./Components/Model";
+import Sidebar from "./Components/Sidebar";
+
 const App = () => {
-  return <h2>Sidebar Starter</h2>;
+  return <main>
+    <Home />
+    <Model />
+    <Sidebar />
+  </main>;
 };
 export default App;
