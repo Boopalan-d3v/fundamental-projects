a07a9507f6304036a8baf0b5ebdec7069d1ec62a
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 22:20:51 +0530

    import NavLinks component

--- strapi-submenus/src/Components/Navbar.jsx
+++ strapi-submenus/src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 import {FaBars} from 'react-icons/fa'
 import { useGlobalContext } from './context'
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
-    const {openSidebar} = useGlobalContext()
+   const {openSidebar} =   useGlobalContext();
   return (
     <nav>
         <div className="nav-center">
             <h3 className='logo'>starpi</h3>
             <button className="toggle-btn" onClick={openSidebar}><FaBars/></button>
-            {/* Nav Links */}
+            <NavLinks/>
         </div>
     </nav>
   )
