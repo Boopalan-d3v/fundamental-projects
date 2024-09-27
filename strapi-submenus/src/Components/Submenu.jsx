6a32e512e3140671235da60eee6d84c78d08b771
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 22:23:22 +0530

    done currentPage functionality

--- strapi-submenus/src/Components/Submenu.jsx
+++ strapi-submenus/src/Components/Submenu.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import sublinks from "../data"
+import { useGlobalContext } from "./context"
 
 const Submenu = () => {
+    const {pageId} = useGlobalContext()
+    const currentPage = sublinks.find(item => item.pageId === pageId)
   return (
-    <div>Submenu</div>
+    <div className={currentPage?'submenu show-submenu': "submenu"}>
+        <h5>{currentPage?.page}</h5>
+        <div className="submenu-links" style={{gridTemplateColumns:currentPage?.links?.length >3  ? '1fr 1fr' : '1fr'}}>
+            {currentPage?.links?.map((link) => {
+              const {id, url, label, icon} = link
+              return <a key={id} href={url}>{icon}{label}</a>
+            })}
+        </div>
+    </div>
   )
 }
 
