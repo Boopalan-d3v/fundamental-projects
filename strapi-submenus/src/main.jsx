0cd676e49794825e7430c5acb5e64664b91cd06e
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 22:15:19 +0530

    import AppProvider component

--- strapi-submenus/src/main.jsx
+++ strapi-submenus/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AppProvider } from './Components/context';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <App/>
     </AppProvider>
   </React.StrictMode>
 );
