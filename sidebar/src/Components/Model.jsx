1891908aeca9d78e54565ac5b2c415fd63d130df
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-27 12:13:59 +0530

    done functionalities

--- sidebar/src/Components/Model.jsx
+++ sidebar/src/Components/Model.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Model = () => {
+  const {isModelOpen, closeModel} = useGlobalContext()
   return (
-    <div>Model</div>
+    <div className={isModelOpen ? 'model-overlay show-model' : 'model-overlay'}>
+      <div className="model-container">
+        <h3>model content</h3>
+        <button className="close-model-btn" onClick={closeModel}><FaTimes/></button>
+      </div>
+    </div>
   )
 }
 
 export default Model
+
+
