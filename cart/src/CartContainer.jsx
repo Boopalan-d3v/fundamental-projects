67c07407a269c1ea0e0c909b217afa5c390d1aa2
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-01 13:35:04 +0530

    cart, clearCart, totalCost by useGlobalContext & fix funtionality

--- cart/src/CartContainer.jsx
+++ cart/src/CartContainer.jsx
@@ -1,7 +1,11 @@
 import CartItem from './CartItem';
-import cartItems from './data';
+import { useGlobalContext } from './context';
+
 const CartContainer = () => {
-  const cartArray = [...cartItems];
+
+  const {cart, clearCart, totalCost} = useGlobalContext()
+
+  const cartArray = Array.from(cart.entries());
 
   if (cartArray.length === 0) {
     return (
@@ -23,7 +27,8 @@ const CartContainer = () => {
       {/* cart items */}
       <div>
         {cartArray.map((cartItem) => {
-          return <CartItem key={cartItem.id} {...cartItem} />;
+          const [id, item] = cartItem
+          return <CartItem key= {id} {...item} />;
         })}
       </div>
       {/* cart footer */}
@@ -31,12 +36,13 @@ const CartContainer = () => {
         <hr />
         <div>
           <h5 className='cart-total'>
-            total <span>$10</span>
+            {/* .toFixed(2) - allows only two digit after '.'  */}
+            total <span>${totalCost.toFixed(2)}</span>
           </h5>
         </div>
         <button
           className='btn btn-hipster'
-          onClick={() => console.log('clear cart')}
+          onClick={clearCart}
         >
           clear cart
         </button>
